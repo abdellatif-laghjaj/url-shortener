6ee15dc69518b1c5c6a00a019fd897ccbbd67237
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-21 02:27:58 +0100

    save btn

--- js/main.js
+++ js/main.js
@@ -45,9 +45,11 @@ shortenBtn.addEventListener('click', function(e) {
                                 //if ajax request status is ok or success
                                 if (xhr2.status === 200 && xhr2.readyState === 4) {
                                     let data = xhr2.response;
+                                    //convert the json to object
+                                    let obj = JSON.parse(data);
                                     notie.alert({
-                                        type: 'success',
-                                        text: data
+                                        type: obj.type,
+                                        text: obj.message
                                     });
                                 }
                             }
--- php/save_url.php
+++ php/save_url.php
@@ -1,3 +1,35 @@
 <?php
     include("config.php");
+    $long_url = mysqli_real_escape_string($conn, $_POST['shorten_url']);
+    $full_url = str_replace(" ", "", $long_url); //remove spaces from url
+    $hidden_url = mysqli_real_escape_string($conn, $_POST['hidden_url']);
+
+    if(!empty($full_url)){
+        $domain = "localhost";
+        //check if user entered a valid url
+        if(preg_match("/{$domain}/i", $full_url) && preg_match("/\//i", $full_url)){
+            $explode_url = explode("/", $full_url);
+            $short_url = end($explode_url);
+            if(!empty($short_url)){
+                $sql = mysqli_query($conn, "SELECT shorten_url FROM url WHERE shorten_url = '$short_url'");
+                if(mysqli_num_rows($sql) == 0){
+                    //update url
+                    $sql2 = mysqli_query($conn, "UPDATE url SET shorten_url = '$short_url' WHERE shorten_url = '$hidden_url'");
+                    if($sql2){
+                        echo "Successfully updated URL";
+                    }else{
+                        echo "Something went wrong";
+                    }
+                }else{
+                    echo "URL already exists";
+                }
+            }else{
+                echo "Please enter a short URL";
+            }
+        }else{
+            echo "You can't edit the domain name of the URL. Please enter a valid URL.";
+        }
+    }else{
+        echo "Please enter a short URL";
+    }
 ?>
